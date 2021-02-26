e4407e6b4cfdbf235c5788f3746c0906c35387ed
Álvaro Castro Castilla <a@fourthbit.com>
2021-02-26 19:44:23 +0200

    Merkle Trees for basic channel (wip)

--- parachain/pallets/commitments/src/lib.rs
+++ parachain/pallets/commitments/src/lib.rs
@@ -1,21 +1,23 @@
 #![cfg_attr(not(feature = "std"), no_std)]
+use itertools::Itertools;
+use std::collections::HashMap;
 
-use sp_std::prelude::*;
-use frame_support::{
-	decl_module, decl_storage, decl_event, decl_error,
-	weights::Weight,
-	dispatch::DispatchResult,
-};
-use sp_io::offchain_index;
-use sp_core::{H160, H256, RuntimeDebug};
-use sp_runtime::{
-	traits::{Hash, Zero},
-	DigestItem
-};
-use codec::{Encode, Decode};
-use artemis_core::{ChannelId, MessageCommitment};
-use ethabi::{self, Token};
+use artemis_core::{BasicMessageCommitment, ChannelId, IncentivizedMessageCommitment};
+use codec::{Decode, Encode};
 use enum_iterator::IntoEnumIterator;
+use ethabi::{self, Token};
+use frame_support::{
+    decl_error, decl_event, decl_module, decl_storage, dispatch::DispatchResult, weights::Weight,
+};
+use sp_core::{RuntimeDebug, H160, H256};
+use sp_io::offchain_index;
+use sp_runtime::{
+    traits::{Hash, Zero},
+    DigestItem,
+};
+use sp_std::prelude::*;
+
+mod merkle_tree;
 
 #[cfg(test)]
 mod mock;
@@ -26,8 +28,8 @@ mod tests;
 /// Auxiliary [`DigestItem`] to include in header digest.
 #[derive(Encode, Decode, Copy, Clone, PartialEq, RuntimeDebug)]
 pub enum AuxiliaryDigestItem {
-	/// A batch of messages has been committed.
-	Commitment(ChannelId, H256)
+    /// A batch of messages has been committed.
+    Commitment(ChannelId, H256),
 }
 
 impl<T> Into<DigestItem<T>> for AuxiliaryDigestItem {
@@ -36,131 +38,180 @@ impl<T> Into<DigestItem<T>> for AuxiliaryDigestItem {
     }
 }
 
-
 /// Wire-format for committed messages
 #[derive(Encode, Decode, Clone, PartialEq, RuntimeDebug)]
 pub struct Message {
-	/// Target application on the Ethereum side.
-	target: H160,
-	/// A nonce for replay protection and ordering.
-	nonce: u64,
-	/// Payload for target application.
-	payload: Vec<u8>,
+    /// Target application on the Ethereum side.
+    target: H160,
+    /// A nonce for replay protection and ordering.
+    nonce: u64,
+    /// Payload for target application.
+    payload: Vec<u8>,
 }
 
 pub trait Config: frame_system::Config {
-	/// Prefix for offchain storage keys.
-	const INDEXING_PREFIX: &'static [u8];
+    /// Prefix for offchain storage keys.
+    const INDEXING_PREFIX: &'static [u8];
 
-	type Hashing: Hash<Output = H256>;
+    type Hashing: Hash<Output = H256>;
 
-	type Event: From<Event> + Into<<Self as frame_system::Config>::Event>;
+    type Event: From<Event> + Into<<Self as frame_system::Config>::Event>;
 }
 
 decl_storage! {
-	trait Store for Module<T: Config> as Commitments {
-		/// Interval between committing messages.
-		Interval get(fn interval) config(): T::BlockNumber;
+    trait Store for Module<T: Config> as Commitments {
+        /// Interval between committing messages.
+        Interval get(fn interval) config(): T::BlockNumber;
 
-		/// Messages waiting to be committed.
-		pub MessageQueues: map hasher(identity) ChannelId => Vec<Message>;
-	}
+        /// Basic channel messages waiting to be committed.
+        BasicMessageQueue get(fn basic_mq): Vec<(T::AccountId, Message)>;
+
+        /// Messages waiting to be committed.
+        IncentivizedMessageQueue get(fn incentivized_mq): Vec<Message>;
+    }
 }
 
 decl_event! {
-	pub enum Event {
+    pub enum Event {
 
-	}
+    }
 }
 
 decl_error! {
-	pub enum Error for Module<T: Config> {}
+    pub enum Error for Module<T: Config> {}
 }
 
 decl_module! {
-	pub struct Module<T: Config> for enum Call where origin: T::Origin {
-		type Error = Error<T>;
+    pub struct Module<T: Config> for enum Call where origin: T::Origin {
+        type Error = Error<T>;
 
-		fn deposit_event() = default;
+        fn deposit_event() = default;
 
-		// Generate a message commitment every [`Interval`] blocks.
-		//
-		// The commitment hash is included in an [`AuxiliaryDigestItem`] in the block header,
-		// with the corresponding commitment is persisted offchain.
-		fn on_initialize(now: T::BlockNumber) -> Weight {
-			if (now % Self::interval()).is_zero() {
-				Self::commit()
-			} else {
-				0
-			}
-		}
-	}
+        // Generate a message commitment every [`Interval`] blocks.
+        //
+        // The commitment hash is included in an [`AuxiliaryDigestItem`] in the block header,
+        // with the corresponding commitment is persisted offchain.
+        fn on_initialize(now: T::BlockNumber) -> Weight {
+            if (now % Self::interval()).is_zero() {
+                Self::commit()
+            } else {
+                0
+            }
+        }
+    }
 }
 
 impl<T: Config> Module<T> {
+    // Generate a key for offchain storage
+    fn offchain_key(channel_id: ChannelId, hash: H256) -> Vec<u8> {
+        (T::INDEXING_PREFIX, channel_id, hash).encode()
+    }
 
-	// Generate a key for offchain storage
-	fn offchain_key(channel_id: ChannelId, hash: H256) -> Vec<u8> {
-		(T::INDEXING_PREFIX, channel_id, hash).encode()
-	}
+    // TODO: return proper weight
+    fn commit() -> Weight {
+        let mut weight = Self::commit_for_basic_channel();
+        weight + Self::commit_for_incentivized_channel()
+    }
 
-	// TODO: return proper weight
-	fn commit() -> Weight {
-		let mut weight: Weight = 0;
+    fn commit_for_basic_channel() -> Weight {
+        let all_messages: Vec<(T::AccountId, Message)> = <Self as Store>::BasicMessageQueue::get();
+        if all_messages.len() == 0 {
+            return 0;
+        }
 
-		for channel_id in ChannelId::into_enum_iter() {
-			weight += Self::commit_for_channel(channel_id);
-		}
+        //
+        // TODO: benchmark this grouping approach vs a sorting in-place solution
+        //
+        let subcommitments = all_messages
+            .into_iter()
+            .map(|(acc, v)| (acc.encode(), v))
+            .into_group_map_by(|x| x.0.clone())
+            .into_iter()
+            .map(|(_, msgs)| {
+                let msgs = msgs
+                    .into_iter()
+                    .map(|(_, msg)| msg)
+                    .collect::<Vec<Message>>();
+                Self::encode_commitment(&msgs[..])
+            });
 
-		weight
-	}
+        let subcom_hashes = subcommitments
+            .into_iter()
+            .map(|c| <T as Config>::Hashing::hash(&c));
 
-	fn commit_for_channel(channel_id: ChannelId) -> Weight {
-		let messages: Vec<Message> = <Self as Store>::MessageQueues::take(channel_id);
-		if messages.len() == 0 {
-			return 0
-		}
+        // 1. Generate Merkle Tree (stripped down generate_merkle_proof)
 
-		let commitment = Self::encode_commitment(&messages);
-		let commitment_hash = <T as Config>::Hashing::hash(&commitment);
+        // 2. Deposit log with Merkle Tree Root
 
-		let digest_item = AuxiliaryDigestItem::Commitment(channel_id, commitment_hash.clone()).into();
-		<frame_system::Module<T>>::deposit_log(digest_item);
+        // 3. Store the messages and/or the merkle proofs off-chain
 
-		offchain_index::set(&Self::offchain_key(channel_id, commitment_hash), &messages.encode());
+        0
+    }
 
-		0
-	}
+    fn commit_for_incentivized_channel() -> Weight {
+        let messages: Vec<Message> = <Self as Store>::IncentivizedMessageQueue::get();
+        if messages.len() == 0 {
+            return 0;
+        }
 
-	// ABI-encode the commitment
-	fn encode_commitment(commitment: &[Message]) -> Vec<u8> {
-		let messages: Vec<Token> = commitment.iter()
-			.map(|message|
-				Token::Tuple(vec![
-					Token::Address(message.target),
-					Token::Uint(message.nonce.into()),
-					Token::Bytes(message.payload.clone())
-				])
-			)
-			.collect();
-		ethabi::encode(&vec![Token::Array(messages)])
-	}
+        let commitment = Self::encode_commitment(&messages);
+        let commitment_hash = <T as Config>::Hashing::hash(&commitment);
+
+        let digest_item =
+            AuxiliaryDigestItem::Commitment(ChannelId::Incentivized, commitment_hash.clone())
+                .into();
+        <frame_system::Module<T>>::deposit_log(digest_item);
+
+        offchain_index::set(
+            &Self::offchain_key(ChannelId::Incentivized, commitment_hash),
+            &messages.encode(),
+        );
+
+        0
+    }
+
+    // ABI-encode the commitment
+    fn encode_commitment(commitment: &[Message]) -> Vec<u8> {
+        let messages: Vec<Token> = commitment
+            .iter()
+            .map(|message| {
+                Token::Tuple(vec![
+                    Token::Address(message.target),
+                    Token::Uint(message.nonce.into()),
+                    Token::Bytes(message.payload.clone()),
+                ])
+            })
+            .collect();
+        ethabi::encode(&vec![Token::Array(messages)])
+    }
 }
 
-impl<T: Config> MessageCommitment for Module<T> {
+impl<T: Config> BasicMessageCommitment<T::AccountId> for Module<T> {
+    fn add_basic(
+        _account_id: T::AccountId,
+        _target: H160,
+        _nonce: u64,
+        _payload: &[u8],
+    ) -> DispatchResult {
+        Ok(())
+    }
+}
 
-	// Add a message for eventual inclusion in a commitment
-	// TODO (Security): Limit number of messages per commitment
-	//   https://github.com/Snowfork/polkadot-ethereum/issues/226
-	fn add(channel_id: ChannelId, target: H160, nonce: u64, payload: &[u8]) -> DispatchResult {
-		MessageQueues::append(
-			channel_id,
-			Message {
-				target,
-				nonce,
-				payload: payload.to_vec()
-			}
-		);
-		Ok(())
-	}
+impl<T: Config> IncentivizedMessageCommitment for Module<T> {
+    // Add a message for eventual inclusion in a commitment
+    // TODO (Security): Limit number of messages per commitment
+    //   https://github.com/Snowfork/polkadot-ethereum/issues/226
+    fn add_incentivized(target: H160, nonce: u64, payload: &[u8]) -> DispatchResult {
+        // TODO
+
+        // MessageQueues::append(
+        //     channel_id,
+        //     Message {
+        //         target,
+        //         nonce,
+        //         payload: payload.to_vec(),
+        //     },
+        // );
+        Ok(())
+    }
 }
--- parachain/primitives/core/src/lib.rs
+++ parachain/primitives/core/src/lib.rs
@@ -6,41 +6,39 @@
 #![allow(unused_variables)]
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use artemis_ethereum::Log;
 use frame_support::dispatch::{DispatchError, DispatchResult};
 use sp_core::H160;
-use artemis_ethereum::Log;
 
-pub mod types;
 pub mod assets;
+pub mod types;
 
-pub use types::{
-	Message,
-	Proof,
-	ChannelId,
-	MessageId,
-	SourceChannelConfig,
-	SourceChannel,
-};
+pub use types::{ChannelId, Message, MessageId, Proof, SourceChannel, SourceChannelConfig};
 
 pub use assets::{AssetId, MultiAsset, SingleAsset};
 /// A trait for verifying messages.
 ///
 /// This trait should be implemented by runtime modules that wish to provide message verification functionality.
 pub trait Verifier {
-	fn verify(message: &Message) -> Result<Log, DispatchError>;
+    fn verify(message: &Message) -> Result<Log, DispatchError>;
 }
 
 /// Outbound submission for applications
 pub trait SubmitOutbound {
-	fn submit(channel_id: ChannelId, target: H160, payload: &[u8]) -> DispatchResult;
+    fn submit(channel_id: ChannelId, target: H160, payload: &[u8]) -> DispatchResult;
+}
+
+pub trait BasicMessageCommitment<AccountId> {
+    fn add_basic(account_id: AccountId, target: H160, nonce: u64, payload: &[u8])
+        -> DispatchResult;
 }
 
 /// Add a message to a commitment
-pub trait MessageCommitment {
-	fn add(channel_id: ChannelId, target: H160, nonce: u64, payload: &[u8]) -> DispatchResult;
+pub trait IncentivizedMessageCommitment {
+    fn add_incentivized(target: H160, nonce: u64, payload: &[u8]) -> DispatchResult;
 }
 
 /// Dispatch a message
 pub trait MessageDispatch<MessageId> {
-	fn dispatch(source: H160, id: MessageId, payload: &[u8]);
+    fn dispatch(source: H160, id: MessageId, payload: &[u8]);
 }
